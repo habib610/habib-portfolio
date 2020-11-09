5fe9854f7ad88f1482b2dfe7d361de178b952c78
habib610 <habib54562@gmail.com>
2020-11-10 01:40:22 +0600

    text animation

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -1,31 +1,42 @@
 import { Container, Grid, makeStyles, Typography } from '@material-ui/core';
+import Typical from 'react-typical'
 import React from 'react';
 import { motion } from "framer-motion"
-const useStyles = makeStyles(theme=>({
-    root:{
+const useStyles = makeStyles(theme => ({
+    root: {
         paddingTop: ' 60px',
         fontFamily: 'Poppins',
-        '& .MuiTypography-h2':{
-        fontWeight: '700',
-        fontSize: '60px',
-        color: 'white',
+        // WebkitTextFillColor: 'transparent',
+        // WebkitBackgroundClip: 'text',
+
+        // background: -webkit-linear-gradient(#eee, #333);
+        // -webkit-background-clip: text
+//   -webkit-text-fill-color: transparent
+        '& .MuiTypography-h2': {
+            fontWeight: '700',
+            fontSize: '60px',
+            color: 'white',
         },
-        '& .MuiTypography-h4':{
+        '& .MuiTypography-h4': {
             fontWeight: '700',
             fontFamily: 'Poppins',
             marginBottom: '20px',
-            color:"#fe3e57",
-            [theme.breakpoints.down("sm")]:{
-                fontSize: '30px',
+            background: "-webkit-linear-gradient(45deg,#11FFBD, #AAFFA9)",
+            WebkitTextFillColor: 'transparent',
+            WebkitBackgroundClip: 'text',
+            
+            [theme.breakpoints.down("sm")]: {
+                fontSize: '26px',
+                fontWeight: '800'
             }
         },
-        '& .MuiTypography-body2':{
+        '& .MuiTypography-body2': {
             color: "white"
         }
     },
-    hero:{
+    hero: {
         width: 300,
-        [theme.breakpoints.up("md")]:{
+        [theme.breakpoints.up("md")]: {
             width: 450,
         }
     }
@@ -34,36 +45,43 @@ const useStyles = makeStyles(theme=>({
 const HeaderMain = () => {
     const classes = useStyles();
     return (
-         
+
         <div className={classes.root} >
- <Container style={{position: 'relative'}}>
-            <Grid  container alignItems="center" spacing={2} justify="center">
-            <Grid item md={1}>
-            </Grid>
-                <Grid item   md={6} >
-                <Typography variant="body2" component="p">
-                WELCOME TO MY WORLD
+            <Container style={{ position: 'relative' }}>
+                <Grid container alignItems="center" spacing={2} justify="center">
+                    <Grid item md={1}>
+                    </Grid>
+                    <Grid item md={6} >
+                        <Typography variant="body2" component="p">
+                            WELCOME TO MY WORLD
                 </Typography>
-                <Typography animate={{scale: 2}} variant="h2"  component="h5" className={classes.myself}>
-                <motion.div animate={{fontSize: "3rem"}}>
-                Hello, I'm <motion.span animate={{color: "#fe3e57"}} className="myName">Habib</motion.span>
-                </motion.div>
+                        <Typography animate={{ scale: 2 }} variant="h2" component="h5" className={classes.myself}>
+                            <motion.div animate={{ fontSize: "3rem" }}>
+                                Hello, I'm <motion.span animate={{ color: "#fe3e57" }} className="myName">Habib</motion.span>
+                            </motion.div>
+                        </Typography>
+
+                        <Typography variant="h4" color="secondary" component="h6">
+                            <Typical
+                                steps={[
+                                    'Front-End Developer', 2000,
+                                        'Web Developer', 2000,
+                                        500, "Graphic Designer", 1000]}
+                                loop={Infinity}
+                                wrapper="p"
+                            />
+                        </Typography>
+                        <Typography variant="body1" color="secondary" style={{ color: "white" }} component="p">
+                            I love to create Awesome website with more cleaner code. I'm a  specialized in frontend for complex scalable web apps. I always provide error free, flat and creative design as user requirements.  Want to know how I may help your project?
                 </Typography>
-                
-                <Typography variant="h4" color="secondary" component="h6">
-                Front-End Developer
-                </Typography>
-                <Typography variant="body1" color="secondary" style={{color: "white"}} component="p">
-                I love to create Awesome website with more cleaner code. I'm a  specialized in frontend for complex scalable web apps. I always provide error free, flat and creative design as user requirements.  Want to know how I may help your project?
-                </Typography>
+                    </Grid>
+                    <Grid item md={5}>
+                        <img className={classes.hero} src="https://iili.io/3e7x3b.md.png" alt="" />
+                    </Grid>
                 </Grid>
-                <Grid item   md={5}>
-                <img  className={classes.hero} src="https://iili.io/3e7x3b.md.png" alt=""/>
-            </Grid>
-            </Grid>
-        </Container>
+            </Container>
         </div>
-       
+
     );
 };
 
