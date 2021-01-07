f9a080e9e6a4dace98499eb5d801824fee0535fa
habib610 <habib54562@gmail.com>
2021-01-07 15:59:50 +0600

    animation-added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,27 @@
-
 import React from "react";
 import AboutMain from "../About/AboutMain/AboutMain";
 import Blogs from "../Blogs/Blogs";
 import Projects from "../Projects/Projects";
 import Skills from "../Skills/Skills";
 import Header from "./Header/Header";
-
+import Slide from 'react-reveal/Slide';
 const Home = () => {
   return (
     <div>
       <Header></Header>
+      <Slide right>
       <AboutMain></AboutMain>
+        </Slide>
+      <Slide left>
       <Skills/>
+        </Slide>
+        <Slide bottom>
       <Projects/>
-      <Blogs/>
+        </Slide>
+        <Slide right>
+         < Blogs/>
+        </Slide>
+      
     </div>
   );
 };
